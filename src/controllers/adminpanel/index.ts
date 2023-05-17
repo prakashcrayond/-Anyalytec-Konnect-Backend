4d4cbea19053477fc214e695467f7edfb60d0f96
SanthoshSandy04 <santhosh@crayond.co>
2023-05-17 13:39:34 +0530

    create admin added in adminpanel

--- src/controllers/adminpanel/index.ts
+++ src/controllers/adminpanel/index.ts
@@ -3,3 +3,4 @@ export * from "./create_adminpanel";
 export * from "./getLoginLogo";
 export * from "./getHeaderLogo";
 export * from "./update_adminpanel";
+export * from "./create_adminuser";
--- src/routes/v1/public/auth/index.ts
+++ src/routes/v1/public/auth/index.ts
@@ -1,11 +1,23 @@
 import { FastifyPluginAsync } from "fastify";
-import { getLoginLogoController } from "../../../../controllers";
+import {
+  createAdminUserController,
+  getLoginLogoController,
+} from "../../../../controllers";
 
 const auth: FastifyPluginAsync = async (fastify): Promise<void> => {
   fastify.get("/panel", async (request: any, reply) => {
     try {
       const response = await getLoginLogoController();
       reply.code(200).send(response);
+    } catch (error) {
+      reply.code(globalThis.status_codes?.error?.status).send(error);
+    }
+  });
+
+  fastify.post("/createadmin", async (request: any, reply) => {
+    try {
+      const response = await createAdminUserController(request.body, fastify);
+      reply.code(response.status).send(response);
     } catch (error) {
       reply.code(globalThis.status_codes?.error?.status).send(error);
     }
